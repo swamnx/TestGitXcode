b7e617bb83b5acbcb18aa5d542cddbf6bdea8409
Swmnx <No>
2021-04-05 23:24:25 +0300

    Initial Commit

---
+++ Xcode/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Xcode
+//
+//  Created by swamnx on 4.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var test = "myTestFuncWithoutUsage"
+        
+        // Do any additional setup after loading the view.
+    }
+    /// - Do smth with my test func
+    /// - Parameter sender: what?
+    @IBAction func Button1(_ sender: UIButton) {
+        print(myTestFuncWithoutUsage22("444"))
+    }
+    
+    /// <#Description#>
+    /// - Parameter x: <#x description#>
+    /// - Returns: <#description#>
+    func myTestFuncWithoutUsage22(_ x:String) -> [Int] {
+        var a_5 = [Int]()
+        a_5.append(44)
+        return a_5
+    }
+
+
+}
+
